165e6e1c6e9b491ef970cfb85d56d8537609a9cb
Asset-of-Anjan <74532790+Asset-of-Anjan@users.noreply.github.com>
2021-06-19 16:05:49 +0530

    Add files via upload

---
+++ Anjanstar.py
@@ -0,0 +1,17 @@
+import turtle
+sc=turtle.Screen()
+sc.setup(600,600)
+spiral = turtle.Turtle()
+spiral.speed(10)
+sc.bgcolor("black")
+col=["yellow","blue","white","green","red"]
+c=0
+#python coder
+for i in range(50):
+    spiral.forward(i*10)
+    spiral.right(144)
+    spiral.color(col[c])
+    if c==3:
+        c=0
+    else:
+        c+=1
